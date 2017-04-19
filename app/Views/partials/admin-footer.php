fb80c2e8bf1c0e1985c6a670f6656fe1e4e5cd2d
ThibALBERTINI <thibaut.albertini@gmail.com>
2017-04-19 18:31:14 +0200

    ...
    ...

--- app/Views/partials/admin-footer.php
+++ app/Views/partials/admin-footer.php
@@ -33,12 +33,12 @@
       integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
       crossorigin="anonymous"></script>
 
-          <script type="text/javascript">
-// PASSER DES INFOS DE PHP VERS JAVASCRIPT
-var urlAjax = '<?php echo $this->url("default_ajax"); ?>';
-
-          </script>
+        <script type="text/javascript">
+          // PASSER DES INFOS DE PHP VERS JAVASCRIPT
+          var urlAjax = '<?php echo $this->url("default_ajax"); ?>';
+        </script>
         <script type="text/javascript" src="<?php echo $this->assetUrl('js/jquery-3.2.0.min.js') ?>"></script>
+        <script src="http://cdn.ckeditor.com/4.6.2/standard-all/ckeditor.js"></script>
         <script type="text/javascript" src="<?php echo $this->assetUrl('js/jquery.easing.min.js') ?>"></script>
         <script type="text/javascript" src="<?php echo $this->assetUrl('js/bootstrap.js') ?>"></script>
         <script type="text/javascript" src="<?php echo $this->assetUrl('js/script.js') ?>"></script>
